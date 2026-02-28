c0e0ada16d7651206c09e2f9ea632bfc3c14cc2a
SadCube <tezla.xd@gmail.com>
2026-02-28 17:49:29 +0100

    1.1.0

--- main.lua
+++ main.lua
@@ -5,19 +5,57 @@ SMODS.Atlas {
     py = 32
 }
 
+local textures = { "u_seals_seal" }
+
 AltTexture({
     key = 'seal',
     set = 'Seal',
     original_sheet = true,
     localization = false,
     path = 'seals.png',
+    loc_txt = { name = "Vanilla" },
 })
+
+if next(SMODS.find_mod("GSPhanta")) then
+    AltTexture({
+        key = "phanta_seals",
+        set = "Seal",
+        path = "phanta_seals.png",
+        loc_txt = { name = "Phanta" },
+        keys = { "phanta_ghostseal" },
+        display_pos = "phanta_ghostseal"
+    })
+    textures[#textures + 1] = "u_seals_phanta_seals"
+end
+
+if next(SMODS.find_mod("plantain")) then
+    AltTexture({
+        key = "plantain_seals",
+        set = "Seal",
+        path = "plantain_seals.png",
+        loc_txt = { name = "Plantain" },
+        keys = { "pl_lavender" },
+        display_pos = "pl_lavender_seal"
+    })
+    textures[#textures + 1] = "u_seals_plantain_seals"
+end
+
+if next(SMODS.find_mod("ortalab")) then
+    AltTexture({
+        key = "ortalab_seals",
+        set = "Seal",
+        path = "ortalab_seals.png",
+        loc_txt = { name = "Ortalab" },
+        keys = { "ortalab_cyan", "ortalab_magenta" },
+    })
+    textures[#textures + 1] = "u_seals_ortalab_seals"
+end
 
 TexturePack {
     key = 'u_seals',
-    textures = {'u_seals_seal'},
+    textures = textures,
     loc_txt = {
         name = 'Unique Seals',
-        text = {'Unique seal symbols'}
+        text = { 'Unique seal symbols' }
     }
 }
